0ebdc6c85fcdbfbef39a301ef6f29fe0d299de0d
inglap-f15u <inglaph@gmail.com>
2024-02-21 01:29:16 +0000

    Limpio comentario en app.js

--- js/app.js
+++ js/app.js
@@ -1,33 +1,6 @@
-// Las "llaves" de encryptación que utilizaremos son las siguientes:
-
-// La letra "e" es convertida para "enter"
-// La letra "i" es convertida para "imes"
-// La letra "a" es convertida para "ai"
-// La letra "o" es convertida para "ober"
-// La letra "u" es convertida para "ufat"
-
-// Requisitos:
-
-// Debe funcionar solo con letras minúsculas
-// No deben ser utilizados letras con acentos ni caracteres especiales
-// Debe ser posible convertir una palabra para la versión encryptada también devolver una palabra encryptada para su versión original.
-// Por ejemplo:
-
-// "gato" => "gaitober"
-// gaitober" => "gato"
-
-// La página debe tener campos para
-// inserción del texto que será encryptado o desencryptado, y el usuario debe poder escoger entre as dos opciones.
-// El resultado debe ser mostrado en la pantalla.
-// Extras:
-
-// Un botón que copie el texto encryptado/desencryptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.
-// Tenemos un periodo de tiempo de cuatro semanas para desarrollar el proyecto y vamos a trabajar con el sistema ágil de desarrollo, utilizando el Trello de la siguiente forma:
-
-
 const arrEncrypt = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
-const arrAccentedCharacters = [['á', 'a'], ['é', 'e'], ['í', 'i'], ['ó', 'o'], ['ú', 'u'], ['ñ', 'n']];
-const arrSpecialCharacters = [['!', ''], ['@', ''], ['#', ''], ['$', ''], ['%', ''], ['&', ''], ['/', ''], ['(', ''], [')', ''], ['=', ''], ['?', ''], ['¡', ''], ['¿', ''], ['*', ''], ['-', ''], ['_', ''], ['+', ''], [';', ''], [':', ''], ['.', ''], [',', ''], ['<', ''], ['>', ''], ['[', ''], [']', ''], ['{', ''], ['}', ''], ['|', ''], ['"', ''], ["'", ''], ['\\', ''], ['^', ''], ['~', ''], ['`', ''], ['¨', ''], ['´', '']];
+
+
 const regexSpecialCharacters = /^[^a-zA-Z0-9\s]+$/; // Expresión regular para caracteres especiales
 const regexAccentedCharacters = /^[áéíóúñ]+$/; // Expresión regular para caracteres acentuados
 const regexUppercaseCharacters = /^[A-Z]+$/; // Expresión regular para caracteres acentuados
@@ -39,7 +12,6 @@ const $pOutText = document.querySelector('#p-outText');
 const $divBtnCopy = document.querySelector('#div-btn--copy');
 const $divResultMsj = document.querySelector('#div-result-mjs');
 const $iconPaste = document.querySelector('.icon-paste');
-// let $pOutTextInit;
 
 
 
@@ -94,9 +66,6 @@ function initialState(status) {
     }
     // cambios si el estado es false
     if (status === false) {
-        console.log("Entre al else");
-        console.log("status: " + status);
-        console.log("$pOutTextInit: " + $pOutTextInit);
         $pOutTextInit.innerHTML = "";
         $imgDiamond.querySelector('.img-diamond');
         $imgDiamond.remove();
@@ -157,7 +126,6 @@ function decryptCharacter(letter) {
     let objDecryptedLetter = {};
     for (let i = 0; i < arrEncrypt.length; i++) {
         let masterKey = arrEncrypt[i];
-        // console.log("masterKey: " + masterKey[1] + " length: " + masterKey[1].length);
         if (letter === masterKey[1][0]) {
             objDecryptedLetter.letter = masterKey[0];
             objDecryptedLetter.length = masterKey[1].length;
@@ -167,9 +135,7 @@ function decryptCharacter(letter) {
         objDecryptedLetter.letter = letter;
         objDecryptedLetter.length = 1;
     }
-    console.log("objDecrypted => Letter: " + objDecryptedLetter.letter + " length: " + objDecryptedLetter.length);
 
-    // AQUI VOY >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     return objDecryptedLetter;
 }
 
@@ -177,20 +143,13 @@ function decryptText(text, controlIndex = 0) {
     if (controlIndex < text.length) {
         for (let i = controlIndex; i < text.length; i++) {
             if (text[controlIndex] !== undefined) {
-                console.group("i: " + i);
-                console.log("controlIndex: " + controlIndex);
-                console.log("letra a evaluad: " + text[controlIndex]);
-                console.log("text.length: " + text.length);
                 objDecrypted = decryptCharacter(text[controlIndex]);
                 decriptedText += objDecrypted.letter;
                 controlIndex += objDecrypted.length;
-                console.log("decriptedText: " + decriptedText);
             }
         }
         decryptText(text, controlIndex);
     }
-    console.log(decriptedText);
-    console.groupEnd();
     return decriptedText;
 }
 
@@ -235,20 +194,14 @@ $iconPaste.addEventListener('click', (e) => {
 // Agrega un evento input al textarea
 $txaInText.addEventListener('input', function (e) {
     e.preventDefault();
-    // Convierte el texto a minúsculas y actualiza el valor del textarea
     this.value = this.value.toLowerCase();
 });
 
-
-
-
-
-// Funciones Principal *******************************
+// Funciones Principales *******************************
 if (initialStatus) {
     initialState(initialStatus);
 
 }
-// intialSt
 
 function exeEncryptDecrypt(f) {
     let textToEncryptDecrypt = $txaInText.value;
@@ -256,8 +209,6 @@ function exeEncryptDecrypt(f) {
     $pOutText.textContent = "";
     decriptedText = "";
 
-
-    // verifico que el texto no tenga caracteres especiales, acentuados o mayúsculas
     let verify = verifyText(textToEncryptDecrypt);
     if (verify) {
         alert("El texto no puede contener caracteres especiales, acentuados o mayúsculas");
@@ -275,7 +226,6 @@ function exeEncryptDecrypt(f) {
         addBtnCopy();
         let $btnCopy = document.querySelector('#btn-copy');
 
-        // agrego el evento al botón de copiar
         $btnCopy.addEventListener('click', (e) => {
             e.preventDefault();
             copyResultText();
